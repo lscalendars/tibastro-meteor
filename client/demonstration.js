bd804c3a12656603b1c3b0463a17b8a7ba72ece1
Jérémy FRERE <frere.jeremy@gmail.com>
2014-12-26 13:11:45 +0100

    First commit

---
+++ client/demonstration.js
@@ -0,0 +1,47 @@
+CalEvents = new Meteor.Collection('calevents');
+
+Router.map(function () {
+  this.route('home', {
+    path: '/',
+  });
+	this.route('calendar');
+});
+
+if (Meteor.isClient) {
+	Template.navbar.helpers({
+	  activeIfTemplateIs: function (template) {
+      var currentRoute = Router.current();
+      return currentRoute && template === currentRoute.route.getName() ? 'active' : '';
+    }
+	});
+	Template.calendar.helpers({
+    calendarOptions: {
+      // Standard fullcalendar options
+      height: 700,
+      hiddenDays: [ 0 ],
+      slotDuration: '01:00:00',
+      minTime: '08:00:00',
+      maxTime: '19:00:00',
+      lang: 'fr',
+      // Function providing events reactive computation for fullcalendar plugin
+      events: function(start, end, timezone, callback) {
+	        var events = [];
+	      calEvents = CalEvents.find();
+	      calEvents.forEach(function(evt){
+	        events.push({id:evt._id,title:evt.title,start:evt.start,end:evt.end});
+	      })
+	      callback(events);
+      },
+      // Optional: id of the calendar
+      id: "calendar1",
+      // Optional: Additional classes to apply to the calendar
+      addedClasses: "col-md-8",
+      // Optional: Additional functions to apply after each reactive events computation
+      autoruns: [
+        function () {
+          console.log("user defined autorun function executed!");
+        }
+      ]
+    },
+  });
+}
